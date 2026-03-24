327bd4fcc32d2fe0f87133aaf9c1130aec00383b
Yusuf Dauda <yusufdauda41@gmail.com>
2026-03-24 22:21:52 +0100

    Update PWA generated files and sync lockfile

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.oe6nkg0t1ic"
+    "revision": "0.ccct7r3p4sc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
