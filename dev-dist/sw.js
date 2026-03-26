55f82063f23616fd58651f17b8510b5f00b3496c
Yusuf Dauda <yusufdauda41@gmail.com>
2026-03-26 09:40:11 +0100

    Restore hierarchical member registration and latest dashboard updates

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.u4dqh7eap6g"
+    "revision": "0.5gdthfn5q2o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
